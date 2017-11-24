59c99fefb40bc16a6e81769bb9c9a0faccb7c48d
Jahed <jahed.public@gmail.com>
2017-11-24 23:40:30 +0000

    fix: APIGatewayEventResult body is optional.

--- src/lambda/types.ts
+++ src/lambda/types.ts
@@ -1,4 +1,4 @@
-import { APIGatewayEvent, APIGatewayEventRequestContext, ProxyResult } from 'aws-lambda'
+import { APIGatewayEvent, APIGatewayEventRequestContext } from 'aws-lambda'
 
 export interface LambdaCallback<R> {
     (error: Error | null, response: R): void
@@ -11,6 +11,13 @@ export interface LambdaHandler<E, C, R> {
 // API Gateway
 
 export type APIGatewayEventContext = APIGatewayEventRequestContext
-export type APIGatewayEventResult = ProxyResult
+export interface APIGatewayEventResult {
+    statusCode: number;
+    headers?: {
+        [header: string]: boolean | number | string;
+    },
+    body?: string;
+    isBase64Encoded?: boolean;
+}
 
 export interface APIGatewayLambdaHandler extends LambdaHandler<APIGatewayEvent, APIGatewayEventContext, APIGatewayEventResult> {}
